6349bea640c831f13e1eab1dd6e52ad9553c5409
apoorvapriyaa <98655703+apoorvapriyaa@users.noreply.github.com>
2022-03-24 17:30:56 +0530

    Update linearsearch.cpp

--- Week 1/linearsearch.cpp
+++ Week 1/linearsearch.cpp
@@ -1,2 +1,30 @@
 #include<iostream>
 using namespace std;
+
+int main()
+{
+    int t;
+    cin>>t;
+    for(int i=0; i<t; i++)
+    {
+        int n, j;
+        cin>>n;
+        int arr[n];
+        for(j=0; j<n; j++)
+        {
+            cin>>arr[j];
+        }
+        int key;
+        cin>>key;
+        for(j=0; j<n; j++)
+        {
+            if(arr[j]==key)
+            {
+                cout<<"Present "<<j+1;
+                break;
+            }
+        }
+        if(j==n)
+        cout<<"Not Present "<<j;
+    }
+}
