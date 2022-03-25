7320c64f2981509aaf8afb8bda0864872832a19d
apoorvapriyaa <98655703+apoorvapriyaa@users.noreply.github.com>
2022-03-25 10:32:46 +0530

    Create exponentialsearch.cpp

---
+++ Week 1/exponentialsearch.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+using namespace std;
+
+int linearSearch(int arr[], int i, int n, int key)
+{
+    for(i=0; i<n; i++)
+    {
+        if(arr[i]==key)
+        return i;
+    }
+    return -1;
+}
+int binarySearch(int arr[], int left, int right, int key)
+{
+    while(left<=right)
+    {
+        int mid= left+ (right-left)/2;
+        if(arr[mid]==key)
+        {
+            return mid;
+        }
+        else if(arr[mid]<key)
+        {
+            left=mid+1;
+        }
+        else
+        right=mid-1;
+    }
+    return -1;
+}
+
+int exponentialSearch(int arr[], int n, int key)
+{
+    if(arr[0]==key)
+    return 0;
+    int i=1;
+    while(i<n && arr[i]<=key)
+    {
+        return i*=2;
+    }
+    return linearSearch(arr, int (i/2), n, key);
+    //return binarySearch(arr, int (i/2), min(i, n-1), key);
+}
