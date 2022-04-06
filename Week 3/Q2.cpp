dcbeddcc8d2e755ed1dffc6dc2aefc9680c7d839
apoorvapriyaa <98655703+apoorvapriyaa@users.noreply.github.com>
2022-04-06 21:56:26 +0530

    Create Q2.cpp

---
+++ Week 3/Q2.cpp
@@ -0,0 +1,53 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void swap(int *a, int *b)
+{
+    int temp=*a;
+    *a=*b;
+    *b=temp;
+}
+
+void selectionSort(int arr[], int n)
+{
+    int c=0, s=0, min_idx;
+    for(int i=0; i<n-1; i++)
+    {
+        min_idx=i;
+        for(int j=i+1; j<n; j++)
+        {
+            c++;
+            if(arr[j]<arr[min_idx])
+            {
+                min_idx=j;
+            }
+        }
+    s++;
+    int temp=arr[min_idx];
+    arr[min_idx]=arr[i];
+    arr[i]=temp;
+    }
+    for(int i=0; i<n; i++)
+        {
+            cout<<arr[i]<<" ";
+        }
+        cout<<endl;
+        cout<<"comparisons= "<<c<<endl;
+        cout<<"swaps= "<<s<<endl;
+}
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        int a[n];
+        for(int i=0; i<n; i++)
+        {
+            cin>>a[i];
+        }
+        selectionSort(a, n);
+    }
+}
